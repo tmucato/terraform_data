bec285fcaa85d13a4b4d1b0ce8321c55ed1e3091
tmucato <tomasmucato@gmail.com>
2023-04-03 06:29:41 +0200

    resource added

--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
 terraform {
     required_providers {
         aws = {
-        source = "hashicorp/aws"
+            source = "hashicorp/aws"
         }
     }
 
     required_version = "1.3.7"
+}
+
+provider "aws" {
+    region =  "eu-south-1"
+}
+
+resource "aws_instance" "angular_server" {
+  
+ami = "ami-"
+instance_type = "t2.micro"
+
+
 }
